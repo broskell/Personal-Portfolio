ba01005f80982cdce9a20cfaef8d8903bef8428c
Kellampalli Saathvik <saathvik.kp@gmail.com>
2025-11-10 11:23:15 +0530

    Added JS console to contactMe

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("This Portfolio isn't ready yet!!")
+
+function submitForm() {
+    let name = document.getElementById("r-name").value;
+    let mail = document.getElementById("r-mail").value;
+    let msg = document.getElementById("r-msg").value;
+
+    alert('Message sent successfully! I\'ll get back to you soon.');
+    console.log(`Name: ${name} \n Mail: ${mail} \n Message: ${msg}`)
+}
